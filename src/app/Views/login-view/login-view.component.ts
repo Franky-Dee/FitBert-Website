0f1340453f78583fda468d182d1bae96ed631f01
Franco Du Plessis <67424625+Franky-Dee@users.noreply.github.com>
2023-05-15 18:32:07 +0200

    Changes to styling + Routing
    Routing is added without authentication to let the user go from login to landing without the need to enter details.
    
    The styling of the login and landing pages are also change

--- src/app/Views/login-view/login-view.component.ts
+++ src/app/Views/login-view/login-view.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'fbw-login-view',
+  selector: 'app-login-view',
   templateUrl: './login-view.component.html',
   styleUrls: ['./login-view.component.css']
 })
 export class LoginViewComponent {
+  constructor(private router: Router) {}
 
+  login(event: Event) {
+    event.preventDefault();
+    // Add your login logic here, then navigate to the new page
+    this.router.navigateByUrl('/landing');
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginViewComponent } from './Views/login-view/login-view.component';
+import { LandingViewComponent } from './Views/landing-view/landing-view.component';
 
 const routes: Routes = [
-  { path: '', component: LoginViewComponent }
+  { path: '', component: LoginViewComponent },
+  { path: 'landing', component: LandingViewComponent }, // Add the route for the new page
+  // other routes
 ];
 
 @NgModule({
