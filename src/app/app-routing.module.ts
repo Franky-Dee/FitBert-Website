a9089bfeb6a2b78226af85091d72f0a0d4bb362f
Franco Du Plessis <67424625+Franky-Dee@users.noreply.github.com>
2023-05-15 09:30:54 +0200

    Streamline CSS + AppRouting
    Changes to the CSS to make it more steamline and changing the app-routing.module.ts to launch the login page when the app is built

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginViewComponent } from './Views/login-view/login-view.component';
+
+const routes: Routes = [
+  { path: '', component: LoginViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
