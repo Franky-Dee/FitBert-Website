6a6a9ab61e3bc964c36f042fb471e28476a80a7c
Franco Du Plessis <67424625+Franky-Dee@users.noreply.github.com>
2023-08-15 16:49:32 +0200

    Select word feature
    The ability for a user to select a word from entered text has been added

--- src/app/Components/question/question.component.ts
+++ src/app/Components/question/question.component.ts
@@ -1,10 +1,21 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-question', 
+  selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent {
-  @Input() questionNumber: number = 1;
+  @Input() questionNumber : number = 0;
+  paragraph: string = '';
+  showZoom: boolean = true;
+  selectedWord: string = '';
+
+  selectWord(word: string) {
+    this.selectedWord = word;
+  }
+
+  getWordsArray() {
+    return this.paragraph.split(' ');
+  }
 }
