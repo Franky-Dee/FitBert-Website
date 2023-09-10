91bd62074c95e21c81f332812e570ae62f59b09d
Franco Du Plessis <67424625+Franky-Dee@users.noreply.github.com>
2023-09-10 11:42:59 +0200

    Initial Backend creation
    Initial open ai backend creation

--- Frontend/src/app/Views/landing-view/landing-view.component.ts
+++ Frontend/src/app/Views/landing-view/landing-view.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'fbw-landing-view',
@@ -7,5 +7,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-view.component.css']
 })
 export class LandingViewComponent {
+  userInput: string = '';
+  generatedText: string = '';
+  apiUrl: string = 'http://localhost:5000/api/process-text'; // Update this URL if needed
 
+  constructor(private http: HttpClient) {}
+
+  sendUserInput(): void {
+    // Create a JSON object with the user's input
+    const requestBody = {
+      user_text: this.userInput
+    };
+
+    // Make a POST request to your Flask backend
+    this.http.post<any>(this.apiUrl, requestBody).subscribe(
+      (response) => {
+        this.generatedText = response.generated_text;
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
 }
---
+++ backend/QuizGenieBackEnd.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import openai
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+openai.api_key = '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'
+CORS(app)
+
+@app.route('/')
+def serve_angular_app():
+    return app.send_static_file('index.html')
+
+@app.route('/api/process-text', methods=['POST'])
+def process_text():
+    user_text = request.json['user_text']
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=user_text,
+            max_tokens=100,  # Adjust as needed
+        )
+        return jsonify({"generated_text": response.choices[0].text})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
