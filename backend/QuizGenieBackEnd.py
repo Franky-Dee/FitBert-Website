ecbe383ba009e87924147c5874319bc3d5505ee0
Franco Du Plessis <67424625+Franky-Dee@users.noreply.github.com>
2023-09-14 09:35:51 +0200

    Intergrating model

--- backend/QuizGenieBackEnd.py
+++ backend/QuizGenieBackEnd.py
@@ -4,12 +4,14 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-openai.api_key = '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'
+openai.api_key = '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'
 CORS(app)
+
 
 @app.route('/')
 def serve_angular_app():
     return app.send_static_file('index.html')
+
 
 @app.route('/api/process-text', methods=['POST'])
 def process_text():
@@ -17,12 +19,14 @@ def process_text():
     try:
         response = openai.Completion.create(
             engine="text-davinci-002",
-            prompt=user_text,
-            max_tokens=100,  # Adjust as needed
+            prompt="Create 5 difficult multiple-choice quiz questions based on the following textbook information:" + user_text +
+            "Each multiple-choice quiz question must be in the following format: Question 1. (Then the contents of the question),A: Distractor 1.B: Distractor 2.C: Distractor 3.D: Distractor 4. E: Distractor 5.Answer: A. (Answer clarification).The distractors must be close in meaning to each other. ",
+            max_tokens=1000,  # Adjust as needed
         )
         return jsonify({"generated_text": response.choices[0].text})
     except Exception as e:
         return jsonify({"error": str(e)})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
